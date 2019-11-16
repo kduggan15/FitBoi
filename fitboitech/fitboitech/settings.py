146a63dffea5b2e7a05747dece856c0e9c0840b1
Jonathan Tran <43768831+jontran1@users.noreply.github.com>
2019-11-15 20:19:07 -0500

    Python Migrations

--- fitboitech/fitboitech/settings.py
+++ fitboitech/fitboitech/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fitboi_photo.apps.FitboiPhotoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
