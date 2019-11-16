cf672c52ea345c24d6e629628debc479678f4a0d
msalamanski <msalamonski@dismail.de>
2019-11-16 03:08:12 -0500

    edited script

--- clear_DB.sh
+++ clear_DB.sh
@@ -2,4 +2,5 @@
 rm -rf fitboitech/fitboi_photo/__pycache__
 rm -rf fitboitech/fitboi_photo/migrations/__pycache__
 rm -rf fitboitech/fitboitech/__pycache__
+rm fitboitech/db.sqlite3
 echo 'cleared all __pycache__'
