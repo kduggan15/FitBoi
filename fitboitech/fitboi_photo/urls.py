d17f0ea063069e83a0762a6e19a5955387b73495
bonliu <cheukbong1210@gmail.com>
2019-11-15 22:29:07 -0500

    Upload page

--- fitboitech/fitboi_photo/urls.py
+++ fitboitech/fitboi_photo/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import *
 
 from . import views
 
 urlpatterns = [
     # ex: /fitboi_photo/
     path('', views.index, name='index'),
+    path('upload', user_image_view, name='image_upload'),
+    path('success', success, name='success'),
 ]
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
