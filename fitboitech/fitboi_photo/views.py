0b762268bea6dba5a1a3da7c135fea846c95ce4b
Jonathan Tran <43768831+jontran1@users.noreply.github.com>
2019-11-16 06:21:19 -0500

    Wrote dictionaries tables

--- fitboitech/fitboi_photo/views.py
+++ fitboitech/fitboi_photo/views.py
@@ -5,6 +5,24 @@ from .forms import UserForm
 # Create your views here.
 
 
+
+gain_program = {
+    'Texas Method' : 'https://www.t-nation.com/training/texas-method', 
+    'Madcow 5x5' : 'https://stronglifts.com/madcow-5x5/',
+    '5/3/1' : 'https://www.t-nation.com/workouts/531-how-to-build-pure-strength',
+}
+
+cardio = {
+    'Running' : 'https://rockay.com/blog/how-to-go-from-couch-to-5k-training-plan/',
+    'Swimming' : 'https://www.liveabout.com/workout-plan-for-beginner-swimmers-3169364',
+}
+
+fat_loss = {
+    'Starting Strength' : 'https://startingstrength.com/get-started/programs',
+    'Strong Lifts' : 'https://stronglifts.com/5x5/',
+    'All Pro\'s' : 'https://forum.bodybuilding.com/showthread.php?t=4195843',
+
+}
 def index(request):
 
     if request.method == 'POST':
