6e684930bb1859dee943153f48ed5a6ee130ea18
kieran duggan <kieranduggan15@gmail.com>
2019-11-15 22:53:37 -0500

    First data for model plus main class for python

---
+++ fitboitech/bmi_photo/bmi_class.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
