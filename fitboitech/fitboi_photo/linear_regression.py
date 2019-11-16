5d2192323b79b79df8226b3d81e768d1be8f822f
kieran duggan <kieranduggan15@gmail.com>
2019-11-16 07:14:05 -0500

    Linear regression function implemented

--- fitboitech/fitboi_photo/linear_regression.py
+++ fitboitech/fitboi_photo/linear_regression.py
@@ -7,27 +7,20 @@ import matplotlib.pyplot as plt
 
 from sklearn.metrics import mean_squared_error
 
-bodyfat_df = pd.read_csv('Bodyfat.csv', index_col=0)
-model = train_linear_regression(bodyfat_df,bodyfat_target)
-
 def train_linear_regression(X, y):
-    """Trains and test linear regression model and returns mean_squared_error of
-    y_test and y_predicted.
-    ----------
-    X_train : array-like, shape = [n_samples, n_features]
-        n_samples the number of samples
-        n_features the number of features for each sample.
-
-    y_train : array-like, shape = [n_samples]
-        n_sample target values.
-
-    Return
-        (float) mean_squared_error of (y_test and y_prediction)
-
-
-    """
 
     lr = LinearRegression()
     lr.fit(X, y)
 
     return lr
+
+def predictBF(neck, waist):
+    X = np.array([neck, waist]).reshape(1,-1)
+    y = model.predict(X)
+    return y
+
+bodyfat_df = pd.read_csv('Bodyfat.csv', index_col=0)
+bodyfat_target = bodyfat_df.bodyfat
+bodyfat_df = bodyfat_df[['Neck','Abdomen']]
+model = train_linear_regression(bodyfat_df,bodyfat_target)
+print(predictBF(25,90))
